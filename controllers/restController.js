6e125763e553ddc8a80c20ad398094c6cf54c686
judy1230 <50729317+judy1230@users.noreply.github.com>
2019-11-04 22:00:28 +0800

    show related comments on restaurant page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 const pageLimit = 10
 const restController = {
 	getRestaurants: (req, res) => {
@@ -39,8 +41,12 @@ const restController = {
 	},
 	getRestaurant: (req, res) => {
 		return Restaurant.findByPk(req.params.id, {
-			include: Category
+			include: [
+				Category,
+				{ model: Comment, include: [User] }
+			]
 		}).then(restaurant => {
+			console.log(restaurant.Comments[0].dataValues)
 			return res.render('restaurant', {
 				restaurant: restaurant
 			})
--- models/comment.js
+++ models/comment.js
@@ -7,6 +7,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Comment.associate = function(models) {
     // associations can be defined here
+    Comment.belongsTo(models.Restaurant)
+    Comment.belongsTo(models.User)
   };
   return Comment;
 };
