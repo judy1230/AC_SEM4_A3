d9b141e0c4abba118b74af7b0293323fa0383654
judy1230 <50729317+judy1230@users.noreply.github.com>
2019-11-04 16:31:37 +0800

    add admin categories delete feature

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,7 @@ module.exports = (app, passport) => {
 	app.post('/admin/categories', authenticatedAdmin, categoryController.postCategories)
 	app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
 	app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+	app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
 	//users
 	app.get('/admin/users', authenticatedAdmin, adminController.editUsers)
