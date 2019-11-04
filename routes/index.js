3ce3ab7224b5afc91968ad2475d1868ee42ebbbd
judy1230 <50729317+judy1230@users.noreply.github.com>
2019-11-04 15:56:21 +0800

    add admin categories page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const resController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
+const categoryController = require('../controllers/categoryController.js')
 const multer = require('multer')
-const upload = multer({ dest: 'temp/'})
+const upload = multer({ dest: 'temp/' })
+
 
 module.exports = (app, passport) => {
 	const authenticated = (req, res, next) => {
@@ -14,20 +16,25 @@ module.exports = (app, passport) => {
 		const authenticatedAdmin = (req, res, next) => {
 			if (req.isAuthenticated()) {
 				if (req.user.isAdmin) {	return next()}
-				return res.redirect('/restaurants')
+				return res.redirect('/')
 			}
 			res.redirect('/signin')
 	}
 	//get in login page
-	//app.get('/', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
-	app.get('/', authenticatedAdmin, adminController.getRestaurants)
+	app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
 	app.get('/restaurants', authenticated, resController.getRestaurants)
 	//get in admin
-	app.get('/admin', authenticated, (req, res) => res.render('admin/config'))
+	app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+	app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
 	//admin config
+	//categories
+	app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+
+	//users
 	app.get('/admin/users', authenticatedAdmin, adminController.editUsers)
 	app.get('/admin/setUser/:id', authenticatedAdmin, adminController.putUsers)
+	//restaurants
 	app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 	app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
 	app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
