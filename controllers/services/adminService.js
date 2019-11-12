37af668f5ed91073509486e7b590d907e406bdc6
judy1230 <50729317+judy1230@users.noreply.github.com>
2019-11-12 15:30:31 +0800

    add PUT /api/admin/restaurants/:id refactoring

--- controllers/services/adminService.js
+++ controllers/services/adminService.js
@@ -61,7 +61,57 @@ const adminService = {
 			})
 				.then((restaurant) => {
 					callback({ status: 'success', message: 'restaurant is successfully created' })
-					
+
+				})
+		}
+
+	},
+	putRestaurant: (req, res, callback) => {
+		if (!req.body.name) {
+			callback({ status: 'error', message:"name didn't exist"})
+			//req.flash('error_msg', "name didn't exist")
+			//return res.redirect('back')
+		}
+		const { file } = req
+		if (file) {
+			imgur.setClientID(IMGUR_CLIENT_ID);
+			imgur.upload(file.path, (err, img) => {
+				return Restaurant.findByPk(req.params.id)
+					.then((restaurant) => {
+						restaurant.update({
+							name: req.body.name,
+							tel: req.body.tel,
+							address: req.body.address,
+							opening_hours: req.body.opening_hours,
+							description: req.body.description,
+							image: file ? img.data.link : restaurant.image,
+							CategoryId: req.body.categoryId
+						})
+							.then((restaurant) => {
+								callback({ status: 'success', message:'restaurant was successfully to update'})
+								//console.log('restaurant', restaurant)
+								//req.flash('success_messages', 'restaurant was successfully to update')
+							//	res.redirect('/admin/restaurants')
+							})
+					})
+			})
+		} else {
+			return Restaurant.findByPk(req.params.id)
+				.then((restaurant) => {
+					restaurant.update({
+						name: req.body.name,
+						tel: req.body.tel,
+						address: req.body.address,
+						opening_hours: req.opening_hours,
+						description: req.body.description,
+						image: restaurant.image,
+						CategoryId: req.body.categoryId
+					})
+						.then((restaurant) => {
+							callback({ status: 'success', message:'restaurant was successfully to update'})
+							//req.flash('success_msg', 'restaurant was successfully to update')
+							//res.redirect('/admin/restaurants')
+						})
 				})
 		}
 
