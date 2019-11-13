67a1cfbfa01c4074520eab3e4dfff9b463085c82
judy1230 <50729317+judy1230@users.noreply.github.com>
2019-11-13 14:33:59 +0800

    install dotenv to add JWT_SECRET parameter to process.env & signin by jwt

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const session = require('express-session')
 const passport = require('./config/passport.js')
 const methodOverride = require('method-override')
 const port = process.env.PORT || 3000
+if (process.env.NODE_ENV !== 'production') {      // 如果不是 production 模式
+	require('dotenv').config()                      // 使用 dotenv 讀取 .env 檔案
+}
 //setup handlebars
 app.engine('handlebars', handlebars({
 	defaultLayout: 'main',
--- routes/apis.js
+++ routes/apis.js
@@ -5,6 +5,8 @@ const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
+const userController = require('../controllers/api/userController.js')
+
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
@@ -18,5 +20,7 @@ router.get('/admin/categories/:id', categoryController.getCategories)
 router.put('/admin/categories/:id', categoryController.putCategory)
 router.delete('/admin/categories/:id', categoryController.deleteCategory)
 
+// JWT signin
+router.post('/signin', userController.signIn)
 
 module.exports = router
